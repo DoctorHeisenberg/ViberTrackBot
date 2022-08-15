b39f095fccfce6ab098a10e53a43ac764cfe0197
Misha <mishaknyazzzz@gmail.com>
2022-08-15 13:05:52 +0300

    fix appsettings

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TrackBot4
 {
